33776393bd4c3955da8a33e3d662fd66702eba2d
Adarsh <agkrishnan2003@gmail.com>
2022-08-17 22:47:43 +0530

    Configured database connection

---
+++ api/config/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    define: {
+        timestamps: false
+    }
+});
