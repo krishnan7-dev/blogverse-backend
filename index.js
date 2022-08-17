3093bc3fe2bf4306c1b06702a2447132d2577a4d
Adarsh <agkrishnan2003@gmail.com>
2022-08-17 22:47:15 +0530

    Created server

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Checking database connection
+const db = require('./api/config/database');
+
+db.authenticate()
+    .then(() => {
+        console.log('Database connected successfully');
+    })
+    .catch(err => console.log(err));
+
+const cors = require('cors')
+
+// Importing routes
+const userRoutes = require('./api/routes/userRoutes');
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Creating routes
+app.use('/user', userRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${ PORT }`)
+});
