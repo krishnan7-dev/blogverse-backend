0a88948ecd11ff22368bc9ea6203ddaca9e9e58c
Adarsh <agkrishnan2003@gmail.com>
2022-08-22 21:03:40 +0530

    Added validation

--- api/controllers/createUserController.js
+++ api/controllers/createUserController.js
@@ -1,23 +1,57 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
+const User = require('../models/User');
 const generateID = require('../utils/idGenerator');
+
+const saltRounds = 10;
+
+function ValidateCredentials(username, email, password) {
+    const USERNAME_REGEX = /^[A-Za-z0-9_-]{5,}$/g;
+    const EMAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    const PASSWORD_REGEX = /^[A-Za-z0-9_@#$-]{8,}$/g;
+    
+    if (!USERNAME_REGEX.test(username)) {
+        return { valid: false, error: 'Invalid Username' };
+    }
+
+    if (!EMAIL_REGEX.test(email)) {
+        return { valid: false, error: 'Invalid Email' };
+    }
+
+    if (!PASSWORD_REGEX.test(password)) {
+        return { valid: false, error: 'Invalid Password' };
+    }
+
+    return { valid: true };
+}
 
 router.post('/', (req, res) => {
     const { username, email, password } = req.body;
+
+    const validationResult = ValidateCredentials(username, email, password);
+    if (!validationResult.valid) {
+        return res.json({ err: validationResult.error });
+    }
+
     const id = generateID();
-    User.create({
-        id,
-        username,
-        email,
-        password
-    })
-        .then(() => {
-            res.send('User created successfully');
+
+    bcrypt.hash(password, saltRounds)
+        .then((hash) => {
+            User.create({
+                id: id,
+                username: username,
+                email: email,
+                password: hash
+            })
+                .then(() => {
+                    res.json({ message: 'User created successfully' });
+                })
+                .catch(err => res.json(err));
         })
-        .catch(err => res.json(err));
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
