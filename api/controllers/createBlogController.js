ebd62de7a4f397a2f6f54c12e975567771edc451
Adarsh <agkrishnan2003@gmail.com>
2022-08-24 23:33:17 +0530

    Changed author_id to author

--- api/controllers/createBlogController.js
+++ api/controllers/createBlogController.js
@@ -6,13 +6,13 @@ const Blog = require('../models/Blog');
 const generateID = require('../utils/idGenerator');
 
 router.post('/', (req, res) => {
-    const { title, content, author_id } = req.body;
+    const { title, content, author } = req.body;
     const id = generateID();
     Blog.create({
         id,
         title,
         content,
-        author_id
+        author
     })
         .then(() => res.send('Blog created successfully'))
         .catch(err => res.json(err));
