287aa954b664c7c1697c6050fe928f3fdd347ab0
Adarsh <agkrishnan2003@gmail.com>
2022-08-24 16:26:35 +0530

    Refactored code

--- api/controllers/updateUserController.js
+++ api/controllers/updateUserController.js
@@ -2,17 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/User');
+const generateJwt = require('../utils/generateJwt');
+const ValidateCredentials = require('../validation/validateCredentials');
+
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 router.put('/:id', (req, res) => {
     const { username, email, password } = req.body;
     const { id } = req.params;
-    User.update({ username, email, password }, {
-        where: {
-            id
-        }
-    })
-        .then(() => res.send('User updated successfully'))
-        .catch(err => res.json(err));
+
+    const validationResult = ValidateCredentials(username, email, password);
+    if (!validationResult.valid) {
+        return res.json({ err: validationResult.error });
+    }
+
+    bcrypt.hash(password, saltRounds)
+        .then((hash) => {
+            User.update({ username, email, hash }, {
+                where: {
+                    id
+                }
+            })
+                .then(() => {
+                    const accessToken = generateJwt(id, username, email, password);
+                    return res.json({ accessToken });
+                })
+                .catch(err => res.json(err));
+        })
 });
 
 module.exports = router;
