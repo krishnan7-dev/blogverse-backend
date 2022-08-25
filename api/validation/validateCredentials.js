9c1c4a3f8802412db9b6b498a3bc36a52508ea3a
Adarsh <agkrishnan2003@gmail.com>
2022-08-25 20:52:29 +0530

    Updated validation

--- api/validation/validateCredentials.js
+++ api/validation/validateCredentials.js
@@ -1,6 +1,29 @@
 const User = require('../models/User');
 
-function ValidateCredentials(username, email, password) {
+async function findDuplicateUsername(username) {
+    const result = await User.findAll({ where: { username } })
+        .then((user) => {
+            if (user.length !== 0) {
+                console.log(user);
+                return { valid: false, error: 'Username is taken' };
+            }
+            return { valid: true };
+        });
+    return result;
+}
+
+async function findDuplicateEmail(email) {
+    const result = await User.findAll({ where: { email } })
+        .then(user => {
+            if (user.length !== 0) {
+                return { valid: false, error: 'Email already exists' };
+            }
+            return { valid: true };
+        });
+    return result;
+}
+
+async function ValidateCredentials(username, email, password) {
     const USERNAME_REGEX = /^[A-Za-z0-9_-]{5,}$/g;
     const EMAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
     const PASSWORD_REGEX = /^[A-Za-z0-9_@#$-]{8,}$/g;
@@ -17,20 +40,16 @@ function ValidateCredentials(username, email, password) {
         return { valid: false, error: 'Invalid Password' };
     }
 
-    User.findAll({ where: { username } })
-        .then((user) => {
-            if (user) {
-                return { valid: false, error: 'Username is taken' };
-            }
-        });
+    const duplicateUsernameResult = await findDuplicateUsername(username);
+    if (!duplicateUsernameResult.valid) {
+        return duplicateUsernameResult;
+    }
     
-    User.findAll({ where: { email } })
-        .then(user => {
-            if (user) {
-                return { valid: false, error: 'Email already exists' };
-            }
-        });
-        
+    const duplicateEmailResult = await findDuplicateEmail(email);
+    if (!duplicateEmailResult.valid) {
+        return duplicateEmailResult;
+    }
+
     return { valid: true };
 }
 
