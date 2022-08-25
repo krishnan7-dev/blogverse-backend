dcb000e983164e2106c643f0990d14cda5add9d6
Adarsh <agkrishnan2003@gmail.com>
2022-08-25 22:29:58 +0530

    Testing connection

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+console.log(process.env.DATABASE_URL);
 module.exports = new Sequelize(process.env.DATABASE_URL, {
     pool: {
         max: 5,
