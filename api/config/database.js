b08910e87cf265a3237d95699bcf30022c2bc93e
Adarsh <agkrishnan2003@gmail.com>
2022-08-25 22:39:35 +0530

    Updated database config

--- api/config/database.js
+++ api/config/database.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
-    },
-    ssl: {
-        rejectUnauthorized: false
     }
 });
